7c63eb698ab128f7ff6af0274c427ad852e097b4
George Geeslin <george.geeslin@gmail.com>
2019-03-21 17:51:17 -0500

    more stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import About from "../components/about"
+import "../components/index.scss"
 
 class IndexPage extends React.Component {
   state = {
